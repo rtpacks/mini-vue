70b9f7709444329ef17191957a94447ed5a09951
azin <luckywords@foxmail.com>
2022-07-15 21:02:20 +0800

    配置、重点在于使用webpack打包

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,16 @@ module.exports = {
   mode: 'development',
   devtool: false,
   // devtool: 'inline-cheap-source-map',
-  entry: './src/index.js',
+  entry: './src/index.js', /* 入口文件 */
   output: {
-    filename: 'mini-vue.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: 'mini-vue.js', /* 输出的文件名 */
+    path: path.resolve(__dirname, 'dist'), /* 输出的路径 */
     clean: true,
     // library: 'MiniVue',
     // libraryTarget: 'umd'
+  },
+  resolve: {
+    extensions: ['.js'] /* 配置文件后缀 */
   },
   devServer: {
     contentBase: './src/examples',
