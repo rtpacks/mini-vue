30959c72235aae2fcb66abbd55e6796f161fb611
azin <luckywords@foxmail.com>
2022-07-16 00:32:07 +0800

    重点：vue的响应式是通过get收集依赖，所以仅仅使用某个对象是不可能被收集到的，必须要使用对象的某个|某些属性才会被get捕获收集

--- src/reactivity/reactive.js
+++ src/reactivity/reactive.js
@@ -1,4 +1,4 @@
-import { hasChanged, isObject } from "../utils";
+import { hasChanged, isArray, isObject } from "../utils";
 import { track, trigger } from "./effect";
 
 /**
@@ -36,6 +36,7 @@ export function reactive(target) {
   const proxy = new Proxy(target, {
     get(target, key, receiver) {
       const res = Reflect.get(target, key, receiver);
+      /* 特殊情况六：嵌套的effect，会导致外层的effect丢失！在effect函数中利用栈解决 */
       track(target, key);
       /* 特殊情况四：深层代理，在vue2中所有的对象都被代理了，但是vue3可以选择哪些被代理 */
       // return res;
@@ -43,8 +44,15 @@ export function reactive(target) {
     },
     set(target, key, value, receiver) {
       /* 特殊情况三：只有值变化了才更新，如果前后是相同的值，不进行更新 */
-      if (!hasChanged(target[key], value)) return;
+      if (!hasChanged(target[key], value))
+        return true; /* 注意set需要返回true代表更新成功，否则报错！ */
       const flag = Reflect.set(target, key, value, receiver);
+      
+      /* 特殊情况五：数组的问题，数组长度的问题 数组的key需要手动触发 */
+      if (isArray(target) && key === 'length' && hasChanged(target.length, value)) {
+        trigger(target, "length"); /* 数组的key需要手动触发 */
+      }
+      
       trigger(target, key); /* 必须等对象更新完成后再触发 */
       return flag;
     },
---
+++ src/reactivity/ref.js
@@ -0,0 +1,46 @@
+import { hasChanged, isObject } from "../utils";
+import { track, trigger } from "./effect";
+import { reactive } from "./reactive";
+
+class RefImpl {
+  constructor(value) {
+    this._isRef = true;
+    /* 用私有属性来实现功能，通过简便的写法，即get value() set value()即可完成相应的get set捕获！ */
+    // 如果是一个对象，则用reactive来实现
+    // 无论是基本数据类型还是引用数据类型，最终都是存储在一个对象的_value属性中的
+    this._value = convert(value); /* 需要用convert来转换，否则引用类型的数据无法捕获！ */
+  }
+
+  /* vue3中实现基本数据类型的响应还是Object.definedProperty，注意是基本数据类型！ */
+  get value() {
+    track(this, 'value')
+    return this._value /* 注意：此时返回的是私有属性的值！ */
+  }
+  set value(value) { /* 注意：使用ref包装的对象特别注意没有引用对象属性，不会被加入依赖的！
+    reactive函数同样也是如此，必须要使用对象的某一个属性，单纯使用对象不使用某个对象的属性是不会加入依赖的！*/
+    if (!hasChanged(this._value, value)) {
+      return ;
+    }
+    this._value = convert(value) /* 此时也需要转换，如果传入的数据一个引用类型的数据，那么就需要转换一下！ */
+    trigger(this, 'value') 
+  }
+}
+
+/* ref函数用于记忆基本数据类型，引用数据类型都交给reactive函数 */
+/* 因为基本数据类型存储在栈上，所以难以判断是否为同一个对象，用一个对象实现存储，和reactive一样 */
+export function ref(value) {
+  if (isRef(value)) {
+    return value;
+  }
+  return new RefImpl(value); /* 用一个对象来存储，因为基本数据类型不能在同一个内存地址上，
+  所以在判断是否为同一个变量时应该使用一个对象来存储这些基本数据类型的变量，以方便进行比较是否为同一个变量对象 */
+}
+
+/* ref函数的实现喝reactive函数实现类似，包括特殊的功能等！ */
+export function isRef(value) {
+  return !!(value && value._isRef);
+}
+
+export function convert(value) {
+  return isObject(value) ? reactive(value) : value;
+}
