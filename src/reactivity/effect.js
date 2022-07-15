ebb1bd4837e6c5d03fcec05621b06a9dfdbd9cf8
azin <luckywords@foxmail.com>
2022-07-15 21:03:35 +0800

    实现effect、track、trigger函数，通过get捕获进行依赖的收集，通过set捕获进行依赖的通知运行

---
+++ src/reactivity/effect.js
@@ -0,0 +1,67 @@
+let activeEffect;
+
+/**
+ * 一个对象可以有多个属性
+ * 一个属性可以在多个effect中使用
+ * 一个effect可以同时使用多个不同的对象|对象属性
+ *
+ * 分析依赖关系，确定是对象|对象属性的变化导致effect，
+ * 那么应该这样设计依赖关系结构
+ * AMap：key为target，value为一个BMap 解释：区分不同对象的AMap
+ * BMap：key为对象的不同属性，value为一个CSet 解释：区分一个对象的不同属性
+ * CSet：存储effect 解释：存储所有依赖于某个对象的某个属性的effect，可以使用数组，但是需要进行去重，使用Set更合适
+ * 注意：这些关系结构都是基于：对象|对象属性的变量导致effect的执行
+ */
+let targetMap = new WeakMap(); /* 区分不同的对象 */
+let keyMap; /* 区分同一个对象的不同属性，不同targetMap元素生成不同的keyMap */
+let effectSet; /* 存储所有依赖于某个对象的某个属性的effect */
+
+export function effect(fn) {
+  const effectFn = () => {
+    let result;
+    try {
+      activeEffect =
+        effectFn; /* 当前的函数会被标记，在reactive代理对象的get操作收集依赖时，activeEffect会被收集，此时的fn就是对应变量的依赖项 */
+      result = fn();
+    } catch (error) {
+      /* watchEffect默认调用，用户的函数可能出错，但是不能影响库代码 */
+      console.error(error);
+    } finally {
+      activeEffect =
+        undefined; /* 必须要重新置为null或者undefined，否则在进行判断收集使会出错 */
+      return result
+    }
+  }
+  return effectFn()
+}
+
+/* track 收集依赖 */
+export function track(target, key) {
+  /* 如果不存在activeEffect，返回即可 */
+  if (!activeEffect) {
+    return;
+  }
+
+  /* ================可以使用三元表达式====================== */
+
+  let depsMap = targetMap.get(target)
+  if(!depsMap) {
+    targetMap.set(target, (depsMap = new Map())) /* 另外一种写法 */
+  }
+
+  let deps = depsMap.get(key)
+  if (!deps) {
+    depsMap.set(key, (deps = new Set()))
+  }
+
+  deps.add(activeEffect)
+}
+
+/* trigger 触发依赖 */
+export function trigger(target, key) {
+  /* 更新触发，取出targetMap中的target，keyMap中的effectSet */
+  targetMap
+    .get(target)
+    ?.get(key)
+    ?.forEach((effect) => effect());
+}
