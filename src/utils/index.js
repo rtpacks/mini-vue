3740acfa96fc3c0f92cc6085bcf613df31ecfd9f
azin <luckywords@foxmail.com>
2022-07-20 09:49:45 +0800

    update

--- src/utils/index.js
+++ src/utils/index.js
@@ -7,12 +7,16 @@ export function isFunction(getter) {
   return typeof getter === 'function'
 }
 
+export function isArray(target) {
+  return Array.isArray(target)
+}
+
 export function isString(target) {
   return typeof target === 'string'
 }
 
-export function isArray(target) {
-  return Array.isArray(target)
+export function isNumber(target) {
+  return typeof target === 'number'
 }
 
 export function hasChanged(origin, current) {
