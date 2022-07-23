fdec6e157e061fe2cd167c2ec84dff6becd22afd
azin <luckywords@foxmail.com>
2022-07-23 21:52:14 +0800

    更新：index.js导出对应的函数、变量

--- src/runtime/index.js
+++ src/runtime/index.js
@@ -1,4 +1,4 @@
-export { nextTick } from "./scheduler";
-export { render, mount } from "./renderer";
-export { h, ShapeFlags, Text, Fragment } from "./vnode";
 export { createApp } from "./createApp";
+export { render, mount } from "./renderer";
+export { nextTick } from "./scheduler";
+export { h, ShapeFlags, Text, Fragment } from "./vnode";
