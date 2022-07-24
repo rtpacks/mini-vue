0020183c1143bb36bedb40c9e000a98473db685d
azin <luckywords@foxmail.com>
2022-07-24 15:20:58 +0800

    update

--- src/runtime/index.js
+++ src/runtime/index.js
@@ -2,3 +2,4 @@ export { createApp } from "./createApp";
 export { render, mount } from "./renderer";
 export { nextTick } from "./scheduler";
 export { h, ShapeFlags, Text, Fragment } from "./vnode";
+export { renderList } from "./helper";
